239db0f40cd32e3b7f2e5f341cf681a6e92010eb
Salman Najah <salmannajah123@gmail.com>
2025-10-18 14:08:15 +0530

    fix ci

--- app/javascript/components/Product/Covers/index.tsx
+++ app/javascript/components/Product/Covers/index.tsx
@@ -48,7 +48,7 @@ export const Covers = ({
   );
 
   return (
-    <figure className={classNames("group relative col-[1/-1]", className)} aria-label="Product preview">
+    <figure className={classNames("preview-container relative col-[1/-1]", className)} aria-label="Product preview">
       {closeButton}
       {prevCover ? <PreviewArrow direction="previous" onClick={() => setActiveCoverId(prevCover.id)} /> : null}
       {nextCover ? <PreviewArrow direction="next" onClick={() => setActiveCoverId(nextCover.id)} /> : null}
@@ -102,7 +102,7 @@ const PreviewArrow = ({ direction, onClick }: { direction: "previous" | "next"; 
       className={classNames(
         "absolute top-1/2 z-1 mx-3 h-8 w-8 -translate-y-1/2 items-center justify-center",
         "rounded-full border border-border bg-background",
-        "flex opacity-0 group-hover:opacity-100",
+        "preview-arrow",
         positionClass,
       )}
       onClick={(e) => {
