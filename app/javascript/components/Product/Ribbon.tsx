9a682992c1e5fa1c1cf42c3c85b4967df0901fab
Salman Najah <salmannajah123@gmail.com>
2025-11-02 12:09:41 +0530

    Ribbon component

---
+++ app/javascript/components/Product/Ribbon.tsx
@@ -0,0 +1,16 @@
+import * as React from "react";
+
+export const Ribbon = ({ quantityRemaining }: { quantityRemaining: number | null }) => {
+  if (quantityRemaining === null) return null;
+
+  return (
+    <div
+      className="absolute top-0 left-[-1.464rem] bg-accent border border-border text-sm text-center w-20 -translate-y-full origin-bottom-right -rotate-45"
+      style={{
+        clipPath: 'polygon(calc(1lh + 2 * var(--border-width)) 0, calc(100% - (1lh + 2 * var(--border-width))) 0, 100% 100%, 0 100%)'
+      }}
+    >
+      {quantityRemaining} left
+    </div>
+  );
+};
