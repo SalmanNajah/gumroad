1c07aa0ab4f8ee6ef94ce106553d8cca2bb98241
Salman Najah <salmannajah123@gmail.com>
2025-11-23 15:43:01 +0530

    combine prop types

--- app/javascript/components/ui/ProductCard.tsx
+++ app/javascript/components/ui/ProductCard.tsx
@@ -1,14 +1,12 @@
 import { classNames } from "$app/utils/classNames";
 import * as React from "react";
 
-export const ProductCard = ({
-  children,
-  className,
-  ...props
-}: {
+type BaseProps = {
   children: React.ReactNode;
   className?: string;
-} & React.HTMLAttributes<HTMLElement>) => (
+} & React.HTMLAttributes<HTMLElement>;
+
+export const ProductCard = ({ children, className, ...props }: BaseProps) => (
   <article
     className={classNames(
       "relative bg-background border border-border rounded duration-150 transition-all hover:shadow grid grid-rows-[auto_1fr_auto]",
@@ -20,14 +18,7 @@ export const ProductCard = ({
   </article>
 );
 
-export const ProductCardFigure = ({
-  children,
-  className,
-  ...props
-}: {
-  children?: React.ReactNode;
-  className?: string;
-} & React.HTMLAttributes<HTMLElement>) => (
+export const ProductCardFigure = ({ children, className, ...props }: BaseProps) => (
   <figure
     className={classNames(
       "aspect-square bg-[url('~images/placeholders/product-cover.png')] bg-cover overflow-hidden rounded-t border-b border-border",
@@ -39,14 +30,7 @@ export const ProductCardFigure = ({
   </figure>
 );
 
-export const ProductCardHeader = ({
-  children,
-  className,
-  ...props
-}: {
-  children: React.ReactNode;
-  className?: string;
-} & React.HTMLAttributes<HTMLElement>) => (
+export const ProductCardHeader = ({ children, className, ...props }: BaseProps) => (
   <header
     className={classNames(
       "p-4 grid grid-rows-1 gap-3 border-b border-border",
@@ -58,14 +42,7 @@ export const ProductCardHeader = ({
   </header>
 );
 
-export const ProductCardSection = ({
-  children,
-  className,
-  ...props
-}: {
-  children: React.ReactNode;
-  className?: string;
-} & React.HTMLAttributes<HTMLElement>) => (
+export const ProductCardSection = ({ children, className, ...props }: BaseProps) => (
   <section className={classNames("grid grid-rows-[1fr_auto] gap-0 p-0 lg:gap-8 lg:px-6 lg:py-4", className)} {...props}>
     {children}
   </section>
